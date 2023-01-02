cc57487cdf3dc0d1c89284cb5bdf944118f40ebc
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2023-01-02 15:38:11 +0700

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,27 +1,12 @@
-# import collectdata
-# import solver
+import solver
 
-# collectdata.collectBacktracking(dataset='easysudoku.txt')
-# collectdata.collectConstraintPropagation(dataset='easysudoku.txt')
-# collectdata.collectSimulatedAnnealing(dataset='easysudoku.txt')
-# collectdata.collectBacktracking(dataset='mediumsudoku.txt')
-# collectdata.collectConstraintPropagation(dataset='mediumsudoku.txt')
-# collectdata.collectSimulatedAnnealing(dataset='mediumsudoku.txt')
-# collectdata.collectBacktracking(dataset='toughsudoku.txt')
-# collectdata.collectConstraintPropagation(dataset='toughsudoku.txt')
-# collectdata.collectSimulatedAnnealing(dataset='toughsudoku.txt')
-# collectdata.collectBacktracking(dataset='hardsudoku.txt')
-# collectdata.collectConstraintPropagation(dataset='hardsudoku.txt')
-# collectdata.collectSimulatedAnnealing(dataset='hardsudoku.txt')
-# collectdata.collectBacktracking(dataset='unfairsudoku.txt')
-# collectdata.collectConstraintPropagation(dataset='unfairsudoku.txt')
-# collectdata.collectSimulatedAnnealing(dataset='unfairsudoku.txt')
+grid = solver.inputSudoku('306508400520000000087000031003010080900863005050090600130000250000000074005206300')
+sol = solver.solveBacktracking(grid=grid)
 
-# grid = solver.inputSudoku('230500400006074900000600025000010060002006040000000309040020050107000004590000002')
-# sol = solver.solveSimulatedAnnealing(grid=grid)
-# solver.printGrid(grid)
-# solver.printGrid(sol)
-
+print('Puzzle:')
+solver.printGrid(grid)
+print('Solution:')
+solver.printGrid(sol)
 
 
 
